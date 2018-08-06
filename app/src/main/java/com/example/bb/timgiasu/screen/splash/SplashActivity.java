95aa4ad69cf3f5d97c6def8bddce31a75918b20d
bachbo <tranbach020796@gmail.com>
2018-08-06 20:56:11 +0700

    init prj

---
+++ app/src/main/java/com/example/bb/timgiasu/screen/splash/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.bb.timgiasu.screen.splash;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by BB on 8/5/2018.
+ */
+
+public class SplashActivity extends AppCompatActivity{
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+//    setContentView(R.layout.activity_splash);
+  }
+}
